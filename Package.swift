68d76d65ba4dc3e429abc026d7d77711448ec076
lynnswap <65545348+lynnswap@users.noreply.github.com>
2025-05-26 13:44:35 +0900

    initial

--- Package.swift
+++ Package.swift
@@ -5,17 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "PDDebugKit",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PDDebugKit",
             targets: ["PDDebugKit"]),
     ],
+    dependencies: [
+        // ★ DeviceKitを追加
+        .package(
+            url: "https://github.com/devicekit/DeviceKit.git",
+            from: "5.5.0"
+        )
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PDDebugKit"),
+            name: "PDDebugKit",
+            dependencies: [
+                .product(name: "DeviceKit", package: "DeviceKit")
+            ]
+        ),
         .testTarget(
             name: "PDDebugKitTests",
             dependencies: ["PDDebugKit"]
